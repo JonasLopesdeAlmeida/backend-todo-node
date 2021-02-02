ea30f19dd96e48680017109f0be512433c86b8b8
Jonas Lopes <jmdlopes.almeida@gmail.com>
2021-02-02 17:53:06 +0000

    route task created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
 
-
-server.get('/teste', (req, res) => {
-res.send('WELCOME TO THE BACKEND 2');
-
-});
-
+const TaskRoutes = require('./routes/TaskRoutes');
+server.use('/task', TaskRoutes);
 
 server.listen(3000, () => {
 
