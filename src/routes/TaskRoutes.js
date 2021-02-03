410a907c2483aea20910e7e1121f5eb817f6aa54
Jonas Lopes <jmdlopes.almeida@gmail.com>
2021-02-03 02:35:46 +0000

    route done created

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -8,6 +8,9 @@ const MacaddressValidation = require('../middlewares/MacaddressValidation');
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
 router.get('/:id', TaskController.listone);
+router.delete('/:id', TaskController.delete);
+router.put('/:id/:done', TaskController.done);
+
 router.get('/filter/all', MacaddressValidation, TaskController.listall);
 
 
