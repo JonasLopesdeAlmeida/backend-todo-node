ee0b60b468dff978c32e4d06aa1c65fffe1ebbfe
Jonas Lopes <jmdlopes.almeida@gmail.com>
2021-02-03 01:36:58 +0000

    route listall and macaddress validation

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const TaskValidation = require('../middlewares/TaskValidation');
 const TaskController = require('../controller/TaskController');
+const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 
-router.post('/',TaskValidation, TaskController.create);
-router.put('/:id',TaskValidation, TaskController.update);
+router.post('/', TaskValidation, TaskController.create);
+router.put('/:id', TaskValidation, TaskController.update);
+
+router.get('/filter/all', MacaddressValidation, TaskController.listall);
 
 module.exports = router;
